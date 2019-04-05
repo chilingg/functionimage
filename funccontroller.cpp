2cfc6b0ef97aeef20fbfc439441161f6a3cc40bb
carper <chilingg@qq.com>
2019-04-05 16:28:50 +0800

    First commit

---
+++ funccontroller.cpp
@@ -0,0 +1,11 @@
+#include "funccontroller.h"
+
+funcController::funcController(QWidget *parent)
+    : QMainWindow(parent)
+{
+}
+
+funcController::~funcController()
+{
+
+}
---
+++ funccontroller.h
@@ -0,0 +1,15 @@
+#ifndef FUNCCONTROLLER_H
+#define FUNCCONTROLLER_H
+
+#include <QMainWindow>
+
+class funcController : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    funcController(QWidget *parent = 0);
+    ~funcController();
+};
+
+#endif // FUNCCONTROLLER_H
